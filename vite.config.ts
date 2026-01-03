34bf3fd2b0e2f157268b29f94a08be7b20b65f3b
NateLiu027 <liuhuaize@live.com>
2026-01-03 12:27:12 +0800

    fix: set vercel base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const base = process.env.VERCEL ? '/' : '/xiyouji/';
     return {
-      base: '/xiyouji/',
+      base,
       server: {
         port: 3000,
         host: '0.0.0.0',
